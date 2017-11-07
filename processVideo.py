dfee0aaf7ac0f6e839c3fd442bc219e2a01c3125
RandallWoodall <randalljwoodall@gmail.com>
2017-11-06 18:44:10 -0700

    Config list for cameras

--- processVideo.py
+++ processVideo.py
@@ -1,3 +1,9 @@
 #Process video with opencv
 import numpy as np
-import cv2
+#import cv2
+
+cameraList = open("config.dat", "r")
+cameras = cameraList.readlines()
+cameras = map(str.strip, cameras)
+
+#vidHandle = cv2.VideoCapture('pull in login info from configured .gitignore file')
