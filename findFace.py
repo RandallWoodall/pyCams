74a551db8a92a604ec4a0759df01d11eb2d6be74
RandallWoodall <randalljwoodall@gmail.com>
2018-01-15 14:42:53 -0700

    We have the cameras managing saving data, now we need to search through it all.

--- findFace.py
+++ findFace.py
@@ -1,4 +1,19 @@
 import cv2
 import numpy as np
 
-#We first need to find the oldest new video file available
+# For now, we are going to feed it a file.  When it's working, 
+# we will simply start it and it will process files as available.
+
+cap = cv2.VideoCapture('faceFind.mp4')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    cv2.imshow('frame', gray)
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+           break
+       
+cap.release()
+cv2.destroyAllWindows()
